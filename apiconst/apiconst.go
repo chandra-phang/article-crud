a1737c321a9de56fdeb81be3214cbaa3e36d609a
Chandra Phang <chandra.2@circles.asia>
2023-09-24 12:16:43 +0700

    initial commit

---
+++ apiconst/apiconst.go
@@ -0,0 +1,10 @@
+package apiconst
+
+const (
+	ServiceDocsTitle = "articles-crud"
+
+	ContentTypeHeader = "Content-Type"
+	TextHTML          = "text/html"
+
+	ContentTypeJSON = "application/json"
+)
---
+++ main.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/gorilla/mux"
+	httpSwagger "github.com/swaggo/http-swagger"
+)
+
+type router struct {
+	// lightweight and fast HTTP router for Go web applications
+	server *chi.Mux
+}
+
+func newRouter() *chi.Mux {
+	r := chi.NewRouter()
+	return r
+}
+
+func InitRoutes() {
+	r := newRouter()
+
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/articles", returnAllArticles)
+		r.Get("/articles/{id}", returnSingleArticle)
+		r.Post("/articles", createNewArticle)
+		r.Put("/articles/{id}", updateArticle)
+		r.Delete("/articles{id}", deleteArticle)
+	})
+
+	// Mount Swagger docs and redirect / to the Swagger UI
+	r.Get("/swagger/*", httpSwagger.Handler(
+		httpSwagger.UIConfig(map[string]string{
+			"onComplete": fmt.Sprintf(`() => { document.title = '%s'; }`, apiconst.ServiceDocsTitle),
+		}),
+	))
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/swagger/index.html", http.StatusMovedPermanently)
+	})
+
+	http.ListenAndServe("10000", r)
+}
+
+func handleRequests() {
+	// creates a new instance of a mux router
+	myRouter := mux.NewRouter().StrictSlash(true)
+	// replace http.HandleFunc with myRouter.HandleFunc
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", returnAllArticles).Methods("GET")
+	myRouter.HandleFunc("/articles/{id}", returnSingleArticle).Methods("GET")
+	myRouter.HandleFunc("/articles", createNewArticle).Methods("POST")
+	myRouter.HandleFunc("/articles/{id}", deleteArticle).Methods("DELETE")
+	myRouter.HandleFunc("/articles/{id}", updateArticle).Methods("PUT")
+
+	// finally, instead of passing in nil, we want
+	// to pass in our newly created router as the second
+	// argument
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+
+func main() {
+	fmt.Println("Rest API v2.0 - Mux Routers")
+	Articles = []Article{
+		{ID: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		{ID: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
+	handleRequests()
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+type Article struct {
+	ID      string `json:"id"`
+	Title   string `json:"title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+// let's declare a global Articles array
+// that we can then populate in our main function
+// to simulate a database
+var Articles []Article
+
+func returnAllArticles(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	for _, article := range Articles {
+		if article.ID == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+}
+
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+	// get the body of our POST request
+	// unmarshal this into a new Article struct
+	// append this to our Articles array.
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var article Article
+	json.Unmarshal(reqBody, &article)
+	// update our global Articles array to include
+	// our new Article
+	Articles = append(Articles, article)
+
+	json.NewEncoder(w).Encode(article)
+}
+
+func deleteArticle(w http.ResponseWriter, r *http.Request) {
+	// once again, we will need to parse the path parameters
+	vars := mux.Vars(r)
+	// we will need to extract the `id` of the article we
+	// wish to delete
+	id := vars["id"]
+
+	// we then need to loop through all our articles
+	for index, article := range Articles {
+		// if our id path parameter matches one of our
+		// articles
+		if article.ID == id {
+			// updates our Articles array to remove the
+			// article
+			Articles = append(Articles[:index], Articles[index+1:]...)
+		}
+	}
+}
+
+func updateArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var updatedEvent Article
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(reqBody, &updatedEvent)
+
+	for i, article := range Articles {
+		if article.ID == id {
+
+			article.Title = updatedEvent.Title
+			article.Desc = updatedEvent.Desc
+			article.Content = updatedEvent.Content
+			Articles = append(Articles[:i], article)
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+}
